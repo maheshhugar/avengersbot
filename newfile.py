8caaee776b7de1319d67b80baf5b4226cf3f9926
maheshhugar <54721452+maheshhugar@users.noreply.github.com>
2019-10-29 16:47:18 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,18 @@
+$nomention
+$title[AVENGERS BOT]
+$description[ ]
+$footer[By MAHI#6044]
+$footercolor[#FF1493]
+$image[https://images.app.goo.gl/pbaUfParcSgvoFCD8]
+$rolecolor[#00FFFF]
+$if message starts with '+hi' 
+$reply username with "hello what help do u need from our family"
+$if message starts with '-announce'
+$reply "@everyone including text"
+$if message starts with '+lo'
+$reply with game is starting join fast for fast and best answers by our server with image [ https://images.app.goo.gl/xKhnB7eTtQzLnbex6]
+$if message starts with '+jh'
+$reply game is about to start join us for accurate answers with image[ https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.jeetoh.com%2Fimages%2F2.png&imgrefurl=https%3A%2F%2Fwww.jeetoh.com%2F&tbnid=SLfu-lUFKXTbPM&vet=1&docid=fKGomOjxOt0HFM&w=436&h=551&q=jeetho%20trivia%20photos&hl=en-US&source=sh%2Fx%2Fim]
+$if message starts with '+fp'
+$reply with game is about to begin join us for better accurate answers with image[https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.jeetoh.com%2Fimages%2F2.png&imgrefurl=https%3A%2F%2Fwww.jeetoh.com%2F&tbnid=SLfu-lUFKXTbPM&vet=1&docid=fKGomOjxOt0HFM&w=436&h=551&q=jeetho%20trivia%20photos&hl=en-US&source=sh%2Fx%2Fim]
+]
